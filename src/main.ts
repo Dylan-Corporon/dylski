6605f405a45838e09d61302356ff9c59f42c839a
Dylan Corporon <dylancorporon@gmail.com>
2025-08-07 18:48:38 -0600

    fixes

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,10 @@ import './style.css'
 import './siteStyles.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import { useApiDataStore } from './stores/apiData'
 
 const app = createApp(App)
 const pinia = createPinia()
 app.use(pinia)
 
-const apiStore = useApiDataStore()
-await apiStore.fetchData()
-await apiStore.fetchLogoLineData()
 
 app.mount('#app')
